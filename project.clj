cde948674a56db71dfb7ee581b733a1e2d011bb0
Ivan Kryvoruchko <gildraug@gmail.com>
2014-04-02 15:36:36 +0300

    Update to latest CLJS version.

--- project.clj
+++ project.clj
@@ -10,13 +10,13 @@
   :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/clojurescript "0.0-2199"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [om "0.5.0"]
-                 [com.facebook/react "0.9.0"]
+                 [om "0.5.3"]
+                 [com.facebook/react "0.9.0.1"]
                  [org.webjars/bootstrap "3.1.1"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
   :hooks [leiningen.cljsbuild]
 

e48d56a73756ba759be0f0b85f6556fca0ed658f
Ivan Kryvoruchko <gildraug@gmail.com>
2014-04-24 01:13:17 +0300

    Regenerate token.

--- script/publish_app.sh
+++ script/publish_app.sh
@@ -2,11 +2,9 @@
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   cd $HOME
-  echo "debug" ${GH_TOKEN}
   git clone --quiet https://${GH_TOKEN}@github.com/gsnewmark/beatha > /dev/null
 
   cd beatha
-  git remote set-url origin https://${GH_TOKEN}@github.com/gsnewmark/beatha.git
   lein2 with-profile prod do clean, compile
 
   git checkout gh-pages
@@ -14,5 +12,6 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   git add js/beatha.js
   git commit -m 'Deploy to Github Pages'
 
-  git push -fq origin gh-pages
+  git remote set-url origin https://${GH_TOKEN}@github.com/gsnewmark/beatha.git
+  git push -fq origin gh-pages > /dev/null 2>&1
 fi

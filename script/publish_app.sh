758f630c80d62ed85a5fad2a8cdb1ecfd7ecf1e1
Ivan Kryvoruchko <gildraug@gmail.com>
2014-04-24 00:05:23 +0300

    Fix remote.

--- script/publish_app.sh
+++ script/publish_app.sh
@@ -12,8 +12,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   git add js/beatha.js
   git commit -m 'Deployed to Github Pages'
 
-  git remote rm origin
-  git remote add origin https://gsnewmark:${GH_TOKEN}@github.com/gsnewmark/beatha.git
+  git remote set-url origin "https://${GH_TOKEN}@github.com/gsnewmark/beatha.git"
 
   git push -fq origin gh-pages > /dev/null 2>&1
 fi

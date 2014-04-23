4c354aabd8671d79242b84d5e8f2a2aa1543db26
Ivan Kryvoruchko <gildraug@gmail.com>
2014-04-23 23:24:02 +0300

    Use version 2 of Leiningen in deploy script.

--- script/publish_app.sh
+++ script/publish_app.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
   cd $HOME
   git clone --quiet https://${GH_TOKEN}@github.com/gsnewmark/beatha > /dev/null
 
   cd beatha
-  lein with-profile prod do clean, compile
+  lein2 with-profile prod do clean, compile
 
   git checkout gh-pages
   cp dev-resources/public/js/beatha.js js/beatha.js

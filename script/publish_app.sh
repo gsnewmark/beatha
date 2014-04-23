a20aa9f1175fd5fcdbf91460fd3623f0c0898662
Ivan Kryvoruchko <gildraug@gmail.com>
2014-04-24 00:23:10 +0300

    Fix deploy script.

--- script/publish_app.sh
+++ script/publish_app.sh
@@ -5,14 +5,15 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   git clone --quiet https://${GH_TOKEN}@github.com/gsnewmark/beatha > /dev/null
 
   cd beatha
+  git remote set-url origin "https://${GH_TOKEN}@github.com/gsnewmark/beatha.git"
   lein2 with-profile prod do clean, compile
 
   git checkout gh-pages
+  git pull origin gh-pages
   cp dev-resources/public/js/beatha.js js/beatha.js
   git add js/beatha.js
-  git commit -m 'Deployed to Github Pages'
-
-  git remote set-url origin "https://${GH_TOKEN}@github.com/gsnewmark/beatha.git"
+  git status
+  git commit -m 'Deploy to Github Pages'
 
   git push -fq origin gh-pages > /dev/null 2>&1
 fi
